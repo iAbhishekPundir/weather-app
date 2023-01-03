e1a9899a048a2f83202e95775954b69749038661
Abhishek <pundirabhishek987@gmail.com>
2023-01-03 06:44:57 +0530

    Add: weather api in index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '233891906bmsh35e3a0e66d921a9p1e5b93jsnf2a62dfa6226',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
